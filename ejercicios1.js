bf565fe929de2f69b3ec819c833b49ff45190a11
MerschIT <92496497+MerschIT@users.noreply.github.com>
2022-05-29 21:25:59 -0300

    Se agregaron arrow function y operadores ternarios

--- ejercicios1.js
+++ ejercicios1.js
@@ -7,36 +7,88 @@
 */
 
 // 1 - Longitud de una cadena de texto
+// Resolucion del video con Arrow Function
 
-function miFuncion(parametro) {
-  return parametro.length;
-}
+const contadorTexto = (texto = "") =>
+  !texto
+    ? console.warn("No ingresaste un texto")
+    : console.log(`El texto tiene ${texto.length} caracteres`);
 
-console.log(miFuncion("Hola Mundo"));
+contadorTexto(); // Probamos que funcione la validacion
+contadorTexto("Hola mundo");
+console.log("************* FIN EJ. N° 1 *************");
 
-// 2 - Texto recortado
+// MI SOLUCION
 
-function recortarTexto(texto, fin) {
-  let resultado = texto.slice(0, fin);
-  return resultado;
-}
-console.log(recortarTexto("Hola mi combre es Jose", 7));
+// function miFuncion(texto = "") {
+//   if (!texto) {
+//     console.warn("No ingresaste un texto");
+//   } else {
+//     console.log(`El texto que ingresaste tiene ${texto.length} caracteres`);
+//   }
+//}
+//miFuncion("Hola Mundo");
 
-// 3 - Convertir cada palabra de un array en un indice
-function stringToArray(texto, separador) {
-  let nuevoArray = texto.split(" ");
-  return nuevoArray;
-}
-console.log(stringToArray("Hola mundo soy un texto"));
+////////////////////////////////// 2 - Texto recortado //////////////////////////////////
+// SOLUCION CON OPERADOR TERNARIO
+const recortarTexto = (texto = "", fin) =>
+  !texto
+    ? console.warn("No ingresaste un texto")
+    : console.log(texto.slice(0, fin));
 
-// 4 - Repetir un texto X veces
+recortarTexto("Hola mundo", 7);
+console.log("************* FIN EJ. N° 2 *************");
 
-function repetidor(texto, cantidad) {
-  let textoFinal = "";
-  for (x = 0; x < cantidad; x++) {
-    textoFinal += texto + " ";
+// SOLUCION SIN ARROW FUNCTION
+// function recortarTexto(texto, fin) {
+//   let resultado = texto.slice(0, fin);
+//   return resultado;
+// }
+// console.log(recortarTexto("Hola mi combre es Jose", 7));
+
+//////////////////// 3 - Convertir cada palabra de un array en un indice ////////////////////
+
+const stringToArray = (texto = "", separador) =>
+  !texto
+    ? console.warn("No ingresaste un texto")
+    : console.log(texto.split(separador));
+
+stringToArray("Este es el texto", " ");
+console.log("************* FIN EJ. N° 3 *************");
+
+// SOLUCION SIN OPERADOR TERNARIO
+// function stringToArray(texto, separador) {
+//   let nuevoArray = texto.split(" ");
+//   return nuevoArray;
+// }
+// console.log(stringToArray("Hola mundo soy un texto"));
+
+//////////////////////////////// 4 - Repetir un texto X veces ////////////////////////////////
+// NO SE PUEDE USAR OPERADOR TERNARIO PORQUE NO ES UNA SOLA LINEA DE CODIGO
+
+const repetidor = (texto = "", cantidad = undefined) => {
+  if (texto === "") {
+    console.warn("No ingresaste un texto");
+  } else if (cantidad === undefined) {
+    console.warn("No ingreaste la cantidad de veces");
+  } else {
+    let textoFinal = "";
+    for (x = 0; x < cantidad; x++) {
+      textoFinal += texto + " ";
+    }
+    console.log(textoFinal);
   }
-  return textoFinal;
-}
+};
 
-console.log(repetidor("Hola mundo", 3));
+repetidor("Hola mundo", 5);
+
+// SIN ARROW FUNCTION
+// function repetidor(texto, cantidad) {
+//   let textoFinal = "";
+//   for (x = 0; x < cantidad; x++) {
+//     textoFinal += texto + " ";
+//   }
+//   return textoFinal;
+// }
+
+// console.log(repetidor("Hola mundo", 3));
